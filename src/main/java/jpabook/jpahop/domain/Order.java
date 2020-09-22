02d7d6f301d886798eb43e810ac92eeb20dc1b59
SeoHoJun <ghwns6659@naver.com>
2020-09-22 14:48:47 +0900

    practice project

--- src/main/java/jpabook/jpahop/domain/Order.java
+++ src/main/java/jpabook/jpahop/domain/Order.java
@@ -11,8 +11,21 @@ public class Order {
 	@Column(name = "ORDER_ID")
 	private Long id;
 
-	@Column(name = "MEMBER_ID")
+	@Column(name = "MEMBER_ID") // 객체를 관계형 DB에 맞춰서 설계할 경우의 방식(데이터 중심 설계)
 	private Long memberId;
+	//Member 측의 주문 데이터를 가져오는데 있어, 위와 같이 주문번호 자체를 사용하여 해당 멤버를 찾는것보다
+	//아예 주문 클래스에서 Member 측의 데이터를 곧장 가져올 수 있게끔 만들면 훨씬 간편하고 객체지향적으로
+	//프로그램을 코딩할 수 있다.
+	private Member member;
+
+	public Member getMember() {
+		return member;
+	}
+
+	public void setMember(Member member) {
+		this.member = member;
+	}
+
 	private LocalDateTime orderDate;
 
 	@Enumerated(EnumType.STRING)
--- src/main/java/jpabook/jpahop/jpaMain.java
+++ src/main/java/jpabook/jpahop/jpaMain.java
@@ -1,5 +1,8 @@
 package jpabook.jpahop;
 
+import jpabook.jpahop.domain.Member;
+import jpabook.jpahop.domain.Order;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -15,6 +18,19 @@ public class jpaMain {
 		tx.begin();
 
 		try{
+
+			/*Order order = em.find(Order.class, 1L);
+			Long memberId = order.getMemberId();
+
+			Member member = em.find(Member.class, memberId);*/ // 객체지향 스럽지 않은 방식
+
+			Order order = em.find(Order.class, 1L);
+			Member findMember = order.getMember();
+			// Order 클래스에서 Member 객체를 얻어올 수 있게끔 객체를 선언해두면
+			// 위와 같이 간편하게 Member 정보를 찾아올 수 있다.
+			// Member 객체를 선언해두지 않았을 경우 과정 : 1. 주문 번호 검색 2. 주문에 해당하는 멤버 번호 검색 3. 멤버 검색
+			// Member 객체를 선언해두었을 경우 과정 : 1. 주문 번호 검색 2. 주문 번호에 해당하는 멤버 검색
+			// 즉, 멤버 번호를 찾는과정을 거칠 필요 없이 더 빠르게 멤버 정보를 찾아올 수 있다.
 
 			tx.commit();
 		} catch (Exception e){
