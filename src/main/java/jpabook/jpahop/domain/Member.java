e2ab2569e949c0fbcfb7d10ab613345c3f23a769
SeoHoJun <ghwns6659@naver.com>
2020-09-29 19:23:41 +0900

    practice project

--- src/main/java/jpabook/jpahop/domain/Member.java
+++ src/main/java/jpabook/jpahop/domain/Member.java
@@ -1,6 +1,8 @@
 package jpabook.jpahop.domain;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Member {
@@ -12,6 +14,13 @@ public class Member {
 	private String city;
 	private String street;
 	private String zipcode;
+
+	// 대부분의 경우 Member 에 Order 클래스에 객체를 선언하여 양방향 매핑을 하는 것이 그닥 좋은 설계가 아니다.
+	// 테이블 입장에서 쿼리를 사용한다고 해도 MEMBER_ID 와 같은 외래 키를 통해 결과를 얻던가 하게 된다.
+	// 그런데 Member 를 찾아서 getOrders 를 찾아 주문 내역을 뿌리는 식으로 만들면 설계를 잘못했다고 볼 수도 있다.
+	// 연관관계 매핑을 할 때는 끊어내야 할 관심사를 잘 끊어내는 것이 중요하다.
+	@OneToMany(mappedBy = "member")
+	private List<Order> orders = new ArrayList<>();
 
 	public Long getId() {
 		return id;
@@ -51,5 +60,13 @@ public class Member {
 
 	public void setZipcode(String zipcode) {
 		this.zipcode = zipcode;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
 	}
 }
--- src/main/java/jpabook/jpahop/domain/Order.java
+++ src/main/java/jpabook/jpahop/domain/Order.java
@@ -2,6 +2,8 @@ package jpabook.jpahop.domain;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "ORDERS")
@@ -11,17 +13,32 @@ public class Order {
 	@Column(name = "ORDER_ID")
 	private Long id;
 
-	@Column(name = "MEMBER_ID") // 객체를 관계형 DB에 맞춰서 설계할 경우의 방식(데이터 중심 설계)
-	private Long memberId;
+	//@Column(name = "MEMBER_ID") // 객체를 관계형 DB에 맞춰서 설계할 경우의 방식(데이터 중심 설계)
+	//private Long memberId;
 	//Member 측의 주문 데이터를 가져오는데 있어, 위와 같이 주문번호 자체를 사용하여 해당 멤버를 찾는것보다
 	//아예 주문 클래스에서 Member 측의 데이터를 곧장 가져올 수 있게끔 만들면 훨씬 간편하고 객체지향적으로
 	//프로그램을 코딩할 수 있다.
+
+	@ManyToOne
+	@JoinColumn(name = "MEMBER_ID") // 연관관계 매핑
 	private Member member;
+	// 가급적이면 설계할 때는 단뱡향으로 연관관계를 매핑하자, 양방향은 나중에 필요할 때 매핑해도 늦지않다.
+
+	// Member - Order 관계와는 반대로 Order - OrderItem 관계에서는 양방향 매핑이 비즈니스 적으로 상당히 가치 있다고 볼 수 있다.
+	// 하나의 주문에서 몇가지 상품이 주문 되었는지 내역을 찾을 경우가 많기도 하기 때문
+	@OneToMany(mappedBy = "order")
+	private List<OrderItem> orderItems = new ArrayList<>();
 
 	private LocalDateTime orderDate;
 
 	@Enumerated(EnumType.STRING)
 	private OrderStatus status;
+
+	public void addOrderItem(OrderItem orderItem){ // 편의성 메소드를 만들자.
+		orderItems.add(orderItem);
+		orderItem.setOrder(this);
+		// 양방향 연관관계를 위한 역방향 삽입
+	}
 
 	public Long getId() {
 		return id;
@@ -29,14 +46,6 @@ public class Order {
 
 	public void setId(Long id) {
 		this.id = id;
-	}
-
-	public Long getMemberId() {
-		return memberId;
-	}
-
-	public void setMemberId(Long memberId) {
-		this.memberId = memberId;
 	}
 
 	public Member getMember() {
@@ -62,4 +71,5 @@ public class Order {
 	public void setStatus(OrderStatus status) {
 		this.status = status;
 	}
+
 }
